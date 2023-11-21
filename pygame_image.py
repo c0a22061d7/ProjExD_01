d4b5d34467eaac196fd5fceb477c95e7cdc1e29e
c0a22061d7 <c0a22061d7@edu.teu.ac.jp>
2023-11-21 15:58:12 +0900

    2

--- pygame_image.py
+++ pygame_image.py
@@ -6,9 +6,9 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    kk_img=pg.image.load("ex01/fig/3.png")
-    kk_img=pg.transform.flip(kk_img,True,False)
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
